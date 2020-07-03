b8650fc72bd158f88512868aac2d49f381814a27
Daniel Monteiro <daniel.amaro.monteiro@gmail.com>
2020-07-04 00:17:40 +0100

    6.2

---
+++ parser.py
@@ -0,0 +1,109 @@
+from token import Token, TokenType
+from typing import List
+import expr
+
+class Parser:
+    tokens : List[Token]
+    current : int
+
+    def __init__(self, tokens : List[Token]):
+        self.tokens = tokens
+        self.current = 0
+    
+    def expression(self) -> expr.Expr:
+        return self.equality()
+    
+    def equality(self) -> expr.Expr:
+        expression = self.comparison()
+
+        while self.match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL):
+            operator = self.previous()
+            right = self.comparison()
+            expression = expr.Binary(expression, operator, right)
+        
+        return expression
+    
+    def match(self, *types) -> bool:
+        for _type in types:
+            if self.check(_type):
+                self.advance()
+                return True
+        else:
+            return False
+        
+    def check(self, _type : TokenType) -> True:
+        if self.is_at_end():
+            return False
+        return self.peek().token_type == _type
+    
+    def advance(self) -> Token:
+        if not self.is_at_end():
+            self.current += 1
+        return self.previous()
+    
+    def is_at_end(self) -> bool:
+        return self.peek().token_type == TokenType.EOF
+    
+    def peek(self) -> Token:
+        return self.tokens[self.current]
+
+    def previous(self) -> Token:
+        return self.tokens[self.current]
+    
+    def comparison(self) -> expr.Expr:
+        expression = self.addition()
+        while self.match(TokenType.GREATER, 
+                         TokenType.GREATER_EQUAL,
+                         TokenType.LESS,
+                         TokenType.LESS_EQUAL):
+            operator = self.previous()
+            right = self.addition()
+            expression = expr.Binary(expression, operator, right)
+        
+        return expression
+
+    
+    def addition(self) -> expr.Expr:
+        expression = self.multiplication()
+        while self.match(TokenType.MINUS, TokenType.PLUS):
+            operator = self.previous()
+            right = self.multiplication()
+            expression = expr.Binary(expression, operator, right)
+        
+        return expression
+    
+    def multiplication(self) -> expr.Expr:
+        expression = self.unary()
+        while self.match(TokenType.SLASH, TokenType.STAR):
+            operator = self.previous()
+            right = self.unary()
+            expression = expr.Binary(expression, operator, right)
+        
+        return expression
+    
+    def unary(self) -> expr.Expr:
+        if self.match(TokenType.BANG, TokenType.MINUS):
+            operator = self.previous()
+            right = self.unary()
+            return expr.Unary(operator, right)
+        else:
+            return self.primary()
+    
+    def primary(self) -> expr.Expr:
+        if self.match(TokenType.FALSE):
+            return expr.Literal(False)
+        if self.match(TokenType.TRUE):
+            return expr.Literal(True)
+        if self.match(TokenType.NIL):
+            return expr.Literal(None)
+        
+        if self.match(TokenType.NUMBER, TokenType.STRING):
+            return expr.Literal(self.previous().literal)
+        
+        if self.match(TokenType.LEFT_PAREN):
+            expression = self.expression()
+            self.consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.")
+            return expr.Grouping(expression)
+        
+        
+        
