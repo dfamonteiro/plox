e2438947e0e13eb255e17722825ff893894b0227
Daniel Monteiro <daniel.amaro.monteiro@gmail.com>
2020-07-03 09:55:08 +0100

    4.1

---
+++ scanner.py
@@ -0,0 +1,7 @@
+from typing import List
+
+from token import Token
+
+class Scanner:
+    def scan_tokens(self) -> List[Token]:
+        return []
---
+++ token.py
@@ -0,0 +1,4 @@
+from typing import List
+
+class Token:
+    pass
