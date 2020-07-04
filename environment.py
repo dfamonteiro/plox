d15d4ef5fc765584749797e9f1db25ef69a407d6
Daniel Monteiro <daniel.amaro.monteiro@gmail.com>
2020-07-05 00:57:06 +0100

    8.3

---
+++ environment.py
@@ -0,0 +1,22 @@
+from typing import Dict, Any
+
+import Token
+import interpreter
+
+class Environment:
+    values : Dict[str, Any]
+
+    def __init__(self):
+        self.values = {}
+    
+    def define(self, name : str, value : Any):
+        self.values[name] = value
+    
+    def get(self, name : Token.Token) -> Any:
+        if name.lexeme in self.values:
+            return self.values[name.lexeme]
+        else:
+            raise interpreter.RuntimeError(
+                name, 
+                f"Undefined variable '{name.lexeme}'.'"
+            )
