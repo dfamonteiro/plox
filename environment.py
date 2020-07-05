ba2e17deb5a19aa24a86d8786dd000735509941e
Daniel Monteiro <daniel.amaro.monteiro@gmail.com>
2020-07-05 11:07:16 +0100

    8.5.1

--- environment.py
+++ environment.py
@@ -1,4 +1,4 @@
-from typing import Dict, Any
+from typing import Dict, Any, Union
 
 import Token
 import interpreter
@@ -6,8 +6,9 @@ import interpreter
 class Environment:
     values : Dict[str, Any]
 
-    def __init__(self):
+    def __init__(self, enclosing = None):
         self.values = {}
+        self.enclosing = enclosing
     
     def define(self, name : str, value : Any):
         self.values[name] = value
@@ -15,18 +16,25 @@ class Environment:
     def get(self, name : Token.Token) -> Any:
         if name.lexeme in self.values:
             return self.values[name.lexeme]
-        else:
-            raise interpreter.RuntimeError(
-                name, 
-                f"Undefined variable '{name.lexeme}'.'"
-            )
+
+        if self.enclosing != None:
+            return self.enclosing.get(name)
+
+        raise interpreter.RuntimeError(
+            name, 
+            f"Undefined variable '{name.lexeme}'.'"
+        )
     
     def assign(self, name : Token.Token, value : Any):
         if name.lexeme in self.values:
             self.values[name.lexeme] = value
             return
-        else:
-            raise interpreter.RuntimeError(
-                name,
-                f"Undefined variable '{name.lexeme}'."
-            )
+
+        if self.enclosing != None:
+            self.enclosing.assign(name, value)
+            return
+
+        raise interpreter.RuntimeError(
+            name,
+            f"Undefined variable '{name.lexeme}'."
+        )
